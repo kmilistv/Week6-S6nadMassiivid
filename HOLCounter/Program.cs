da0cabd09cbc709746d5541b572e2e61fe858819
KristinaMilistver <KristinaMilistver@VG10>
2022-04-29 17:36:23 +0300

    add new projects

---
+++ HOLCounter/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu 'h', 'o' ja 'l' tähte on lauses "Hello World!"
+
+            Console.WriteLine("Sisesta Hello:");
+            string Hello = Console.ReadLine();
+            Console.WriteLine("Sisesta World!:");
+            string World = Console.ReadLine();
+
+            string HelloWorld = $"{Hello}{World}".ToLower();
+
+            Console.WriteLine(HelloWorld);
+
+            int hCounter = 0;
+
+            for (int i = 0; i < HelloWorld.Length; i++)
+            {
+                if (HelloWorld[i] == 'h')
+                {
+                    hCounter++;
+                }
+            }
+
+            if (hCounter != 1)
+            {
+                Console.WriteLine($"Lauses Hello World on {hCounter} 'h' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses Hello World on {hCounter} 'h' täht");
+            }
+
+            int oCounter = 0;
+
+            for (int i = 0; i < HelloWorld.Length; i++)
+            {
+                if (HelloWorld[i] == 'o')
+                {
+                    oCounter++;
+                }
+            }
+
+            if (oCounter != 1)
+            {
+                Console.WriteLine($"Lauses Hello World! on {oCounter} 'o' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses Hello World! on {oCounter} 'o' täht");
+            }
+
+            int lCounter = 0;
+
+            for (int i = 0; i < HelloWorld.Length; i++)
+            {
+                if (HelloWorld[i] == 'l')
+                {
+                    lCounter++;
+                }
+            }
+
+            if (lCounter != 1)
+            {
+                Console.WriteLine($"Lauses Hello World! on {lCounter} 'l' tähte");
+            }
+            else
+            {
+                Console.WriteLine($"Lauses Hello World! on {lCounter} 'l' täht");
+            }
+
+        }
+
+    }
+
+}      
+    
+
---
+++ HelloWorldReversed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorldReversed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab lauset "Hello, World!" tagurpidi
+
+            Console.WriteLine("Sisesta Hello, World!:");
+            string HelloWorld = Console.ReadLine();
+
+            for (int i = HelloWorld.Length - 1; i >= 0; i--)
+            {
+                Console.Write(HelloWorld[i]);
+            }
+
+        }
+    }
+}
